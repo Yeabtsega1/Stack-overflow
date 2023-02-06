cd36b0118e3c1470678f067a14ba05706e435260
Yeabtsega Behailu <91087178+Yeabtsega1@users.noreply.github.com>
2023-02-06 13:11:39 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import questionRoutes from './routes/Questions.js'
 import answerRoutes from './routes/Answers.js'
 
 const app = express();
+const cors = require("cors");
 dotenv.config();
 app.use(express.json({limit: "30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb", extended : true}))
